5c64c55bda53bceb2350f2b6e5dbfe0d8afcb793
Shorya Singh Thakur <53837949+shorya97@users.noreply.github.com>
2021-01-30 11:57:58 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://admin:admin@cluster0.mdk4w.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "ghrfjvrhhlrlbcyudgcilbsyubviudsblvydbgi"
-}
